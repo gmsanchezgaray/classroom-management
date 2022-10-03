a169d03a16a4a7b77a438e76905426e6ab7f17b9
Gonzalo Sanchez Garay <gmsanchezgaray@gmail.com>
2022-10-02 21:59:25 -0300

    Add Form and Table component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,10 +4,11 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PipesModule } from '../pipes/pipes.module';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [PagesComponent, DashboardComponent],
-  imports: [CommonModule, PipesModule, FormsModule],
+  imports: [CommonModule, PipesModule, FormsModule, SharedModule],
   exports: [PagesComponent],
 })
 export class PagesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TableComponent } from './components/table/table.component';
+import { PipesModule } from '../pipes/pipes.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
-  imports: [CommonModule],
-  exports: [NavbarComponent, SidebarComponent],
+  declarations: [
+    NavbarComponent,
+    SidebarComponent,
+    TableComponent,
+    FormComponent,
+  ],
+  imports: [
+    CommonModule,
+    PipesModule,
+    FormsModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [NavbarComponent, SidebarComponent, TableComponent, FormComponent],
 })
 export class SharedModule {}
